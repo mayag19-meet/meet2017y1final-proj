28523f291d8bd7cf4dc4b447884b08eafce6b989
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-07 17:05:07 +0300

    Stopping here

---
+++ limit.py
@@ -0,0 +1,13 @@
+import turtle
+drawer = turtle.clone()
+turtle.hideturtle()
+drawer.penup()
+drawer.goto(-600, 350)
+drawer.pendown()
+drawer.goto(600, 350)
+drawer.goto(600, -350)
+drawer.goto(-600, -350)
+drawer.goto(-600, 350)
+
+
+
