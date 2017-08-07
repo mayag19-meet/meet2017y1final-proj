74c5bfa2cd76b3121ef0503600b4a6d4fa82c86d
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-07 17:01:19 +0300

    Stopping here

--- finProj.py
+++ finProj.py
@@ -1,15 +1,36 @@
 import turtle
 
-char=turtle.clone
-fireballs=turtle.clone
-healthy_food=turtle.clone
-health_bar=turtle.clone
+
+
+character=turtle.clone()
+fireballs=turtle.clone()
+healthy_food=turtle.clone()
+health_bar=turtle.clone()
+health_bar.shape('square')
+
 
 char_pos = character.pos()
 fireball_pos = []
 healthy_food = []
-health_bar=[]
+new_health = []
 
-START_LENGTH=5
-for i in range (START_LENGTH):
+max_health = 5
+health = max_health
+
+health_list = []
+
+
+for health in health_pos:
+
+if fireball_pos in stick.pos:
+    health_bar = health_bar -1
+
+if stick.pos in healthy_food.pos:
+    if health = max_health:
+        health = 5
+    else:
+        new_health = health + 1
+
+    
+    
     
