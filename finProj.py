0c75cce51e1d9bd96e2b4416a22edb9532e9c12e
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-07 12:30:44 +0300

    Stopping here

---
+++ finProj.py
@@ -0,0 +1,15 @@
+import turtle
+
+char=turtle.clone
+fireballs=turtle.clone
+healthy_food=turtle.clone
+health_bar=turtle.clone
+
+char_pos = character.pos()
+fireball_pos = []
+healthy_food = []
+health_bar=[]
+
+START_LENGTH=5
+for i in range (START_LENGTH):
+    
