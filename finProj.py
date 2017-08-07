e6fd639e21b98e2b569391344aba0044681a14b6
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-07 22:03:28 +0300

    Stopping here

--- finProj.py
+++ finProj.py
@@ -1,5 +1,9 @@
 import turtle
 
+SIZE_X = 800
+SIZE_Y = 500
+turtle.setup(SIZE_X,SIZE_Y)
+
 
 
 character=turtle.clone()
@@ -18,19 +22,22 @@ max_health = 5
 health = max_health
 
 health_list = []
+health_pos = []
+for i in range(max_health):
+    health_pos.append((SIZE_X/2 - 50 -20 * i   , SIZE_Y/2 - 50))
+health_pos.reverse()
 
-
-for health in health_pos:
-
-if fireball_pos in stick.pos:
-    health_bar = health_bar -1
-
-if stick.pos in healthy_food.pos:
-    if health = max_health:
-        health = 5
-    else:
-        new_health = health + 1
-
-    
-    
-    
+##for health in health_pos:
+##
+##if fireball_pos in stick.pos:
+##    health_bar = health_bar -1
+##
+##if stick.pos in healthy_food.pos:
+##    if health = max_health:
+##        health = 5
+##    else:
+##        new_health = health + 1
+##
+##    
+##    
+##    
