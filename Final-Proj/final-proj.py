18430573ab1e16f7c3c167ba35c6eb007de28579
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-09 01:48:10 -0700

    m

--- Final-Proj/final-proj.py
+++ Final-Proj/final-proj.py
@@ -1,25 +1,9 @@
 import turtle
-turtle.register_shape('r1.gif')
-turtle.register_shape('r2.gif')
-turtle.register_shape('5.gif')
-turtle.register_shape('r3.gif')
-turtle.register_shape('r4.gif')
-turtle.register_shape('r5.gif')
-turtle.register_shape('l1.gif')
-turtle.register_shape('l2.gif')
-turtle.register_shape('l3.gif')
-turtle.register_shape('l4.gif')
-turtle.register_shape('l5.gif')
-turtle.bgpic('ted.gif')
 
-TIME_STEP=200
-
-healthbar=turtle.clone()
-healthbar.penup()
-healthbar.goto(150,300)
-healthbar.shape('5.gif')
+turtle.bgpic('bg.gif')
 
 turtle.penup()
+
 
 turtle.goto(0,-118)
 
@@ -32,45 +16,14 @@ direction= UP
 def left():
     global direction
     direction=LEFT
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
     turtle.shape('ln.gif')
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.shape('l1.gif')
-    turtle.goto(x-4,y)
-    turtle.delay(40)
-    turtle.shape('l2.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x-4,y)
-    turtle.delay(40)
-    turtle.shape('l3.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x-4,y)
-    turtle.delay(40)
-    turtle.shape('l4.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x-4,y)
-    turtle.delay(40)
-    turtle.shape('l5.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    turtle.goto(x+4,y)
-    #print('you pressed right')
-    turtle.pos()
-    old_pos=turtle.pos()
+    turtle.goto(x-30,y)
+   
+
     
-    turtle.goto(x+20,y)
     #print(turtle.pos())
 def right():
     global direction
@@ -79,41 +32,9 @@ def right():
     old_pos=turtle.pos()
     x=old_pos[0]
     y=old_pos[1]
-    turtle.shape('r1.gif')
-    turtle.goto(x+4,y)
-    turtle.delay(40)
-    turtle.shape('r2.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x+4,y)
-    turtle.delay(40)
-    turtle.shape('r3.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x+4,y)
-    turtle.delay(40)
-    turtle.shape('r4.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    x=old_pos[0]
-    y=old_pos[1]
-    x=old_pos[0]
-    y=old_pos[1]
-    turtle.goto(x+4,y)
-    turtle.delay(40)
-    turtle.shape('r5.gif')
-    turtle.pos()
-    old_pos=turtle.pos()
-    turtle.goto(x+4,y)
-    #print('you pressed right')
-    turtle.pos()
-    old_pos=turtle.pos()
+    turtle.shape('rn.gif')
+    turtle.goto(x+30,y)
     
-    turtle.goto(x+20,y)
     #print(turtle.pos())
 
 LEFT_ARROW = 'Left'
@@ -126,4 +47,15 @@ turtle.listen()
 
 turtle.register_shape('rn.gif')
 turtle.register_shape('ln.gif')
+
+#if turtle.pos() in fireball_pos:
+	#food_ind = fireball_pos.index(snake.pos())
+ 	#food.clearstamp(food_stamps[food_ind])
+   	#food_pos.pop(food_ind)
+	#food_stamps.pop(food_ind)
+    #make_food()
+    #w=snake.stamp()
+    #pos_list.append(w)
+    #stamp_list.append(w)
+
 turtle.mainloop()
