a16374ec72d9c3821465a2e16551c421dda1f0c0
tamerk19-meet <tamerk19@meet.mit.edu>
2017-08-09 10:26:11 +0300

    Stopping here

--- finProj.py
+++ finProj.py
@@ -1,7 +1,7 @@
 import turtle
 
 SIZE_X = 800
-SIZE_Y = 500
+SIZE_Y = 600
 turtle.setup(SIZE_X,SIZE_Y)
 
 
@@ -18,14 +18,52 @@ fireball_pos = []
 healthy_food = []
 new_health = []
 
-max_health = 5
-health = max_health
+maxhealth = 5
+health = maxhealth
 
 health_list = []
 health_pos = []
-for i in range(max_health):
-    health_pos.append((SIZE_X/2 - 50 -20 * i   , SIZE_Y/2 - 50))
-health_pos.reverse()
+d=50
+a=30
+health1=(800/2 -d,600/2 -d)
+health2=(800/2 -d-a,600/2 -d)
+health3=(800/2 -d-a*2,600/2 -d)
+health4=(800/2 -d-a*3,600/2 -d)
+health5=(800/2 -d-a*4,600/2 -d)
+
+heart=turtle.clone()
+heart.hideturtle()
+heart_pos=[(health1),(health2),(health3),(health4),(health5)]
+heart_stamp_list=[]
+
+heart.penup() 
+
+for position in heart_pos:
+    heart.goto(position)
+    new_heart=heart.stamp()
+    heart_stamp_list.append(new_heart)
+
+
+def lose_heart():
+    current_health=len(heart_stamp_list)
+    if current_health >=1:
+        old_stamp=heart_stamp_list.pop(-1)
+        heart.clearstamp(old_stamp)
+    else:
+        print('you dead')
+        quit()
+
+def gain_health():
+    current_health=len(heart_stamp_list)
+    if current_health!= maxhealth:
+        health.goto(health_pos[current_health])
+        new_heart=heart.stamp()
+        heart_stamp_list.append(new_heart)
+
+
+
+    
+#health_pos.reverse()
 
 ##for health in health_pos:
 ##
@@ -37,7 +75,3 @@ health_pos.reverse()
 ##        health = 5
 ##    else:
 ##        new_health = health + 1
-##
-##    
-##    
-##    
